6085ab4b5bc9bf2c845a34fe8e08ae96d7750500
ortiz92105@gmail.com <ortiz92105@gmail.com>
2020-04-21 16:40:32 -0700

    hard coded locations

---
+++ PieChartBlazor/Data/DataChart.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieChartBlazor.Data
+{
+    public class DataChart
+    {
+        public int Id { get; set; } = 0;
+        public string Name { get; set; } = "";
+        public int TotalVisits { get; set; } = 0;
+        public int Rate { get; set; } = 0;
+        public string RatePercentage => (this.Rate * 100) / this.TotalVisits + "%";
+
+        public List<DataChart> GetDataCharts()
+        {
+            var dataCharts = new List<DataChart>();
+
+            dataCharts.Add(new DataChart() { Id = 1, Name = "Chollas Lake", TotalVisits = 978, Rate = 429 });
+            dataCharts.Add(new DataChart() { Id = 1, Name = "Balboa Park", TotalVisits = 978, Rate = 429 });
+            dataCharts.Add(new DataChart() { Id = 1, Name = "Mission Bay Park", TotalVisits = 1778, Rate = 329 });
+            dataCharts.Add(new DataChart() { Id = 1, Name = "Fiestas Island", TotalVisits = 7778, Rate = 801 });
+            dataCharts.Add(new DataChart() { Id = 1, Name = "Mission Trails", TotalVisits = 455, Rate = 29 });
+
+
+            return dataCharts;
+        }
+
+    }
+}
--- PieChartBlazor/wwwroot/js/Chart.js
+++ PieChartBlazor/wwwroot/js/Chart.js
@@ -1,14 +1,14 @@
-﻿function GeneratePieChart(countryInfos) {
+﻿function GeneratePieChart(dataCharts) {
         am4core.useTheme(am4themes_animated);
         // Themes end
         var chart = am4core.create("chartdiv", am4charts.PieChart3D);
         chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
         chart.legend = new am4charts.Legend();
 
-        chart.data = countryInfos;
+        chart.data = dataCharts;
           
         var series = chart.series.push(new am4charts.PieSeries3D());
-    series.dataFields.value = "deathPercentage";      //lowercase
-        series.dataFields.category = "name  ";
+        series.dataFields.value = "ratePercentage";      //lowercase
+        series.dataFields.category = "name    ";
    // }); // end am4core.ready()
 }
